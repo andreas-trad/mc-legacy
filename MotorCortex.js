2de4ed6ad18f7428817713efa59eb16c92605730
andreas-trad <atrantidis@gmail.com>
2014-05-15 17:28:04 +0300

    no message

--- MotorCortex.js
+++ MotorCortex.js
@@ -1,9 +1,46 @@
 window.MotroCortex = function(options){
     var mc_debug = options.hasOwnProperty('debug')?options.debug:false;
     var globals = [];
+    var events = [];
 
-    var StepCreator = function(){
-        var optionsNames = ["duration", "easing", "delay", "complete", "loop"];
+    var MC = this;
+
+    var optionsNames = ["duration", "easing", "delay", "complete", "loop"];
+
+    var compile = function(topNode){
+        for(var property in topNode.attributes){
+            var globalsRegx = new RegExp(/^@[a-zA-Z0-9\.]*?/);
+
+            if(topNode.attributes.hasOwnProperty(property)){
+                // step 0. Check if global
+                if(globalsRegx.exec(property)){
+                    globals[property] = topNode.attributes[property];
+                } else {
+                    MC.log("warn", "The property " + property + " should begin with the character '@'. It will be ignored!");
+                }
+            }
+        }
+
+        for(var property in topNode.children){
+            // 1.a Split the expression
+            var selectorArray = property.split(":");
+
+            // 1.b Check that the newly created array is at least of length 2
+            if(selectorArray.length < 2){
+                MC.log("error", "The MC selection and event part " + property + " is not valid. Each MC selection string should consist by least a selection string and an event name" +
+                    " separated by the ':' character.");
+            } else {
+                if(!events.hasOwnProperty(selectorArray[selectorArray.length - 1])){
+                    var event = new Event();
+                    event.addThread(selectorArray, topNode.children[property]);
+                    events[selectorArray[selectorArray.length - 1]] = event;
+                } else {
+                    event.addThread(selectorArray, topNode.children[property]);
+                }
+            }
+        }
+
+
 
         this.createStep = function(selector, attributes){
             var properties = {};
@@ -27,21 +64,69 @@ window.MotroCortex = function(options){
         };
     };
 
-    var Thread = function(superNode){
-        var steps = [];
+    var PlainNodeHandler = function(node){
+
     };
 
-    Thread.prototype = (function(){
+    var Step = function(){
+        var functions = [];
+    };
 
+    Step.prototype = (function(){
+        var callbackHandler = {
+
+        };
     }());
+
+    var Thread = function(selector, node){
+        var steps = [];
+        var selectionFunction = this.createSelectionFunction(selector);
+    };
+
+    /*
+    gets the selector in the format selectionString:@data-x>2@index=2:eventName
+    and return a function that when executed will return the items to be animated
+    on event trigger.
+    The available possible formats are:
+     any valid CSS selection
+     triggeringElement
+     not(triggeringElement)
+     expressions
+
+     expressions:
+     1. index (starts with @)
+     @index>x
+     @index<x
+     @index<=x
+     @index>=x
+     @index==x
+     @index in [x,x,x....]
+     @index odd
+     @index even
+     */
+    Thread.prototype.createSelectionFunction = function(selector){
+        var selectorArray = selector.split(":");
+
+        var selectorArrayLength = selectorArray.length;
+        for(var i=0; i<selectorArrayLength - 1; i++){
+            if(selectorArrayLength.indexOf('@') == -1){
+
+            }
+        }
+    };
 
     var Event = function(){
         var threads = [];
+
+        this.addTheThread = function(selector, node){
+            threads.push(new Thread(selector, node));
+        }
     };
 
-    Event.prototype = (function(){
-
-    }());
+    Event.prototype.addThread = function(selector, node){
+        var thread = new Thread();
+        this.addTheThread(selector, node);
+    };
 
 
     var CMSPARSER = new function () {
@@ -93,7 +178,7 @@ window.MotroCortex = function(options){
          *            node. comments: Whether to capture comments. split: Whether to
          *            split each comma separated list of selectors.
          */
-        base.toThreads = function (cssString, args) {
+        base.toJSON = function (cssString) {
             var node = {
                 children: {},
                 attributes: {}
@@ -101,18 +186,12 @@ window.MotroCortex = function(options){
             var match = null;
             var count = 0;
 
-            if (typeof args == 'undefined') {
-                var args = {
-                    ordered: false,
-                    comments: false,
-                    stripComments: false,
-                    split: false
-                };
-            }
-            if (args.stripComments) {
-                args.comments = false;
-                cssString = cssString.replace(commentX, '');
-            }
+            var args = {
+                ordered: false,
+                comments: false,
+                stripComments: false,
+                split: false
+            };
 
             while ((match = altX.exec(cssString)) != null) {
                 if (!isEmpty(match[capComment]) && args.comments) {
@@ -260,16 +339,14 @@ window.MotroCortex = function(options){
                 type: 'GET',
                 async: true,
                 success:function(cssString){
-                    var threads = CMSPARSER.toThreads(cssString);
-
-
+                    var json = CMSPARSER.toJSON(cssString);
+                    compile(json);
                 },
                 error:function(err, textStatus, errorThrown){
                     that.log("error", "The MSS file " + currentlyScanning + " seems to be missing", errorThrown);
                 }
             });
         }
-
-
     }
+
 };
